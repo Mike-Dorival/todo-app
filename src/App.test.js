132e2ee86410f4e37974d6806cae638413e922e0
Mike <mike.dorival@benextcompany.com>
2022-02-23 18:27:45 +0100

    update css App and add test for component App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Render todo by default", () => {
+  it("show by default text Apprendre le CI/CD", () => {
+    render(<App />);
+    const linkElement = screen.getByText("Apprendre le CI/CD");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("show by default text Faire des projets en TS", () => {
+    render(<App />);
+    const linkElement = screen.getByText("Faire des projets en TS");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("show by default text Tester react-query", () => {
+    render(<App />);
+    const linkElement = screen.getByText("Tester react-query");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("show by default text Zustand c'est cool", () => {
+    render(<App />);
+    const linkElement = screen.getByText("Zustand c'est cool");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
